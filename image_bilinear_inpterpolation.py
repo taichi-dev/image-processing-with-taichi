89f1cbe4552e18381393d80113b33b861d386cf6
Zhao Liang <mathzhaoliang@gmail.com>
2022-10-14 01:21:11 +0800

    Update image_bilinear_inpterpolation.py

--- image_bilinear_inpterpolation.py
+++ image_bilinear_inpterpolation.py
@@ -14,18 +14,19 @@ dst = np.zeros(shape_s, dtype=src.dtype)
 
 
 @ti.kernel
-def bilinear_interp(src: ti.types.ndarray(), dst: ti.types.ndarray()):
+def bilinear_interp(src: ti.types.ndarray(element_dim=1),
+                    dst: ti.types.ndarray(element_dim=1)):
     for I in ti.grouped(dst):
-        x, y = I.xy / scale
+        x, y = I / scale
         x1, y1 = int(x), int(y)  # Bottom-left corner
-        x2, y2 = min(x + 1, h - 1), min(y + 1, w - 1)  # Top-right corner
-        Q11 = src[tm.ivec3(x1, y1, I.z)]
-        Q21 = src[tm.ivec3(x2, y1, I.z)]
-        Q12 = src[tm.ivec3(x1, y2, I.z)]
-        Q22 = src[tm.ivec3(x2, y2, I.z)]
+        x2, y2 = min(x1 + 1, h - 1), min(y1 + 1, w - 1)  # Top-right corner
+        Q11 = src[x1, y1]
+        Q21 = src[x2, y1]
+        Q12 = src[x1, y2]
+        Q22 = src[x2, y2]
         R1 = tm.mix(Q11, Q21, x - x1)
         R2 = tm.mix(Q12, Q22, x - x1)
-        dst[I] = ti.u8(ti.round(tm.mix(R1, R2, y - y1)))
+        dst[I] = ti.round(tm.mix(R1, R2, y - y1)).cast(ti.u8)
 
 
 bilinear_interp(src, dst)
