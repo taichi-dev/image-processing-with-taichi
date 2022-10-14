c5eab8dd06526e0845b4794e4f4f485b7ccca557
neozhaoliang <mathzhaoliang@gmail.com>
2022-10-14 16:35:45 +0800

    use range for in transpose

--- image_transpose.py
+++ image_transpose.py
@@ -5,16 +5,15 @@ import taichi as ti
 ti.init()
 
 src = cv2.imread("./images/cat.jpg")
-shape_t = list(src.shape)
-shape_t[0], shape_t[1] = shape_t[1], shape_t[0]
-dst = np.zeros(shape_t, dtype=src.dtype)
+h, w, c = src.shape
+dst = np.zeros((w, h, c), dtype=src.dtype)
 
 
 @ti.kernel
 def transpose(src: ti.types.ndarray(element_dim=1),
               dst: ti.types.ndarray(element_dim=1)):
-    for I in ti.grouped(src):
-        dst[I.yx] = src[I]
+    for i, j in ti.ndrange(h, w):
+        dst[j, i] = src[i, j]
 
 
 transpose(src, dst)
